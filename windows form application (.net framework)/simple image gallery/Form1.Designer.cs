1508f67585bb3c8e4f7960a7ff2fd56b99bd2856
READYonHub <104925582+READYonHub@users.noreply.github.com>
2023-07-26 23:30:25 +0200

    #3 kész, egyszerű képnézegető

---
+++ windows form application (.net framework)/simple image gallery/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace simple_image_gallery
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 426);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "<<<";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.goBack);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(397, 426);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = ">>>";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.goNext);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::simple_image_gallery.Properties.Resources._1;
+            this.pictureBox1.Location = new System.Drawing.Point(92, 62);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Simple Image Gallery";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ windows form application (.net framework)/simple image gallery/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace simple_image_gallery
+{
+    public partial class Form1 : Form
+    {
+
+        int i = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            changeImage(3);
+        }
+
+        private void goNext(object sender, EventArgs e)
+        {
+            i++;
+            if(i>5)
+            {
+                i = 1;
+            }
+            changeImage(i);
+        }
+
+        private void goBack(object sender, EventArgs e)
+        {
+            i--;
+            if (i < 1)
+            {
+                i = 5;
+            }
+            changeImage(i);
+
+        }
+        private void changeImage(int num)
+        {
+            switch(num)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources._1;
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources._2;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources._3;
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources._4;
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources._5;
+                    break;
+
+            }
+
+        }
+
+    }
+}
